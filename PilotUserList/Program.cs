a7effe0fb9fd1f21a866946c9481f7ac5ab0f8f9
ropsar <88557021+ropsar@users.noreply.github.com>
2026-09-18 12:15:23 +0300

    Update Program.cs
    Добавлена переменная license_type в блок данных авторизации

--- PilotUserList/Program.cs
+++ PilotUserList/Program.cs
@@ -49,6 +49,7 @@ namespace PilotConsole
             string databaseName = args.Length > 1 ? args[1] : "FAIRYTALE";
             string login = args.Length > 2 ? args[2] : "RAIN\\k.lapkin";
             string password = args.Length > 3 ? args[3] : "Test123";
+            string license_type = args.Length > 3 ? args[4] : "101";
 
             // ID целевой папки (если Guid.Empty — выгрузка всей базы)
             Guid folderId = new Guid("24e3fc9d-2ab2-47dc-a7ef-af13d65e5dde");
@@ -74,7 +75,7 @@ namespace PilotConsole
                 var serverApi = httpClient.GetServerApi(searchCallback);
 
                 var auth = httpClient.GetAuthenticationApi();
-                auth.Login(databaseName, login, password.EncryptAes(), false, 101);
+                auth.Login(databaseName, login, password.EncryptAes(), false, license_type);
                 Console.WriteLine("Авторизация успешна.");
 
                 var dbInfo = serverApi.OpenDatabase();
